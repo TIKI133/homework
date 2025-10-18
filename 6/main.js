f663ebf7de533c2b2e0f4f76665dd9be6e5c6e13
TIKI133 <dima.tiki2000@mail.ru>
2025-10-18 17:41:49 +0300

    Add files via upload

---
+++ 6/main.js
@@ -0,0 +1,37 @@
+// Задача 1
+const users = [
+    { name: 'Alex', age: 24, isAdmin: false },
+    { name: 'Bob', age: 13, isAdmin: false },
+    { name: 'John', age: 31, isAdmin: true },
+    { name: 'Jane', age: 20, isAdmin: false }
+];
+users.push(
+    { name: 'Ann', age: 19, isAdmin: false },
+    { name: 'Jack', age: 43, isAdmin: true }
+);
+console.log(users)
+
+// Задача 2
+function getUserAverageAge(users) {
+    let sum = 0;
+    users.forEach(({ age }) => {
+        sum += age;
+    });
+    const average = sum / users.length;
+    console.log(average);
+}
+getUserAverageAge(users)
+
+// Задача 4
+function first(array, n) {
+    let result = [];
+    if (n === undefined)
+        return array[0];
+    for (let i = 0; i < n; i++) {
+        result.push(array[i]);
+    }
+    return result;
+}
+console.log(first([1, 2, 3, 4, 5]))
+console.log(first([1, 2, 3, 4, 5], 0))
+
